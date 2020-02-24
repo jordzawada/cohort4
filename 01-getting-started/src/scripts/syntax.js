27185c85636f6b2cbbc0d29cd8b8ffa93709da8e
J Zawada <jordzawada@gmail.com>
2020-02-24 13:54:25 -0700

    100C first half of exercise

---
+++ 01-getting-started/src/scripts/syntax.js
@@ -0,0 +1,80 @@
+// define attributes / variables
+// number
+let x= 5;
+// string
+const greetingPrimary = "Hello you";
+// boolean
+y= false;
+// array
+const arr1 = [1,2,[5,8],"hello", false]
+// dictionary / objects
+const obj1 = {
+    name: "bob",
+    arr: arr1,
+    kids: x,
+}
+// undefined
+let c;
+
+
+// sample if / else
+if (x>5) {
+    return "number is less than 5";
+} else {
+    return "number is probably larger than 5";
+}
+
+// functions
+//  parameters
+const funct1 = (param1,param2) => {
+    return "hello";
+}
+//  returns
+    funct1 (1,2);
+
+
+// arrays
+// add to the front
+arr1.unshift("watermelon");
+// add to the end
+arr1.push("earth");
+// update values
+arr1[2]; //call the vlaue
+arr1[2]=7; //make it 7
+
+
+// loops 
+// for
+for (let i=0;i>8;i++){
+    console.log(i)
+};
+// for/in
+for (a in obj1){
+
+}
+// while
+while (h < 10) {
+  // code block to be executed
+}
+// do while
+do {
+    let lyrics ="the hokey pokey";
+  }
+  while (`it's all about`);
+
+// forEach (with array and function)
+arr1.forEach(function(currentValue, index, arr1), thisValue);
+
+// Objects / Dictionaries
+// declare object
+const obj2 = {
+    name: jerry
+};
+// lookup key to retrieve value
+obj2.name;
+
+const function1 = (x) => {
+    return "hello";
+}
+
+export default function1;
---
+++ 01-getting-started/src/scripts/syntax.test.js
@@ -0,0 +1,6 @@
+import function1 from './syntax'
+
+test ('test 1', () =>{
+    expect(function1(1)).toBe("hello");
+    expect().toBe();
+});
