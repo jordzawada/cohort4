b9b85635bc0a4db34ac5e29f31ba23c8d7500218
J Zawada <jordzawada@gmail.com>
2020-02-24 11:41:15 -0700

    did 100B changes

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -2,9 +2,11 @@
 const functions = {
     
     size: (num) => {
+        if (num < -2) return "extra large"
         if (num < 10) return "small";
         if (num < 20) return "medium";
         return "large";
+        
     },
 
     add: (num1, num2) => {
@@ -13,8 +15,15 @@ const functions = {
 
     subtract: (num1, num2) => {
         return num1 - num2;
-    }
+    },
 
+    isEven: (x) => {
+        if (x%2 === 0) {
+            return true;  
+        } else  {
+            return false;
+        }
+    }
     
 };
 
