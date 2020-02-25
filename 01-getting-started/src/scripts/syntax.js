6842ea61eb83d2609ea777d080f035d959502a71
J Zawada <jordzawada@gmail.com>
2020-02-25 16:39:04 -0700

    made even more changes to 100C

--- 01-getting-started/src/scripts/syntax.js
+++ 01-getting-started/src/scripts/syntax.js
@@ -1,85 +1,135 @@
-// define attributes / variables
-// number
-let x= 5;
-// string
-const greetingPrimary = "Hello you";
-// boolean
-let y= false;
-// array
-const arr1 = [1,2,[5,8],"hello", false]
-// dictionary / objects
-const obj1 = {
-    name: "bob",
-    arr: arr1,
-    kids: x,
-}
-// undefined
-let c;
+        
+    const functions = {    
+        // define attributes / variables
+        // number
+        fun1: () => { 
+        let x=5;
+        return x;
+        },
+        // string
+        fun2: () => { 
+            const greetingPrimary = "Hello you";
+            return greetingPrimary;
+        },
+        // boolean
+        fun3: () => { 
+            let y= false;
+            return y;
+        },
+        // array
+        fun4: () => { 
+            const arr1 = [1,2,[5,8],"hello", false]
+            return length.arr1;
+        },
+
+        // // dictionary / objects
+        fun5: () => { 
+            const obj1 = {
+                name: "bob",
+                arr: [0,1],
+                kids: 4,
+            }
+            return typeof(obj1);
+        },
+
+        fun6: () => { 
+            let c;
+            return c;
+        },        
+
+        fun7: (num) => {
+            // sample if / else
+            if (num%2 != 0){
+            return "odd";
+            } else {
+            return "even"
+            }
+        },
+        fun8: (a,b) => {
+            //  parameters
+            return a+b;
+        },
+
+        
+   // arrays
+        fun9: (x) => {
+            // add to the front
+            let arr1 =[0];
+            arr1.unshift(x);
+            return arr1[0];
+            },
+        fun10: (x) => {
+            // add to the end
+            let arr1=[];
+            arr1.push(x)
+            return arr1[arr1.length-1];
+            },
+        fun11: (x,y) => {
+            // update values
+            let arr1=[5,6,7,8];
+            arr1[x]=y;
+            return arr1[x];
+            },
+    // loops
+        fun12: (x) => {
+            // for
+            let sum =x;
+            for (let i=0;i<x;i++) {
+                sum = sum + i; 
+            }
+            return sum;
+        },
+        fun13: (x) => {
+            // for/in
+            let arr1 = [ 1, 2,3];
+            for (let y in arr1){
+                arr1[y] = x;
+            }
+            return arr1[0];
+        },
+        fun14: (x) => {
+            let h=0;
+            let sum=0;
+            while (h<=x){
+                sum = sum+h;
+                h++;
+            }
+
+            return sum;
+        },
+        fun15: (x) => {
+             //     do while
+            let h="";
+            let i=0;
+            do {
+                i++;
+                h=h + i;
+                
+            } while (i<x);
+
+            return h;
+        },
+
+        helper: (x) => {
+            return x;
+        },
+        fun16: (x) => {
+        //     forEach
+            let arr1=[x];
+            arr1.forEach(functions.helper);
+            return arr1[0] ; 
+           
+       },
+
+       fun17: () => {
+        const obj2 = {
+            name: 'jerry',
+        };
+        return obj2.name;
+           
+       },
 
 
-// sample if / else
-let z=0;
-if (z>5) {
-    console.log( "number is less than 5");
-} else {
-    console.log( "number is probably larger than 5");
-}
+    }
 
-// functions
-//  parameters
-const funct1 = (param1,param2) => {
-    return "hello";
-}
-//  returns
-    funct1 (1,2);
-
-
-// arrays
-// add to the front
-arr1.unshift("watermelon");
-// add to the end
-arr1.push("earth");
-// update values
-arr1[2]; //call the vlaue
-arr1[2]=7; //make it 7
-
-
-// loops 
-// for
-for (let i=0;i>8;i++){
-    console.log(i)
-};
-// for/in
-let a= name;
-for (a in obj1){
-
-}
-// while
-let h=0;
-while (h < 10) {
-  // code block to be executed
-  h++;
-  console.log(h);
-}
-// do while
-// do {
-//     let lyrics ="the hokey pokey";
-//   }
-//   while (lyrics != `it's all about`);
-
-// forEach (with array and function)
-// arr1.forEach(function(currentValue, index, arr1), thisValue);
-
-// Objects / Dictionaries
-// declare object
-const obj2 = {
-    name: 'jerry',
-};
-// lookup key to retrieve value
-obj2.name;
-
-const function1 = (x) => {
-    return "hello";
-}
-
-export default function1;
+    export default functions;
--- 01-getting-started/src/scripts/syntax.test.js
+++ 01-getting-started/src/scripts/syntax.test.js
@@ -1,8 +1,47 @@
-import function1 from './syntax'
+import functions from './syntax'
 
-// test ('test 1', () =>{
-//     expect(function1(1)).toBe("hello");
-//     expect().toBe();
-// });
+test ('testing section 1 stuff', () =>{
+    expect(functions.fun1()).toBe(5);
+    expect(functions.fun2()).toBe("Hello you");
+    expect(functions.fun3()).toBe(false);
+    expect(functions.fun4()).not.toBe(0);
+});
 
-expect().toBe();
+test ('testing section 2 stuff', () =>{
+    expect(functions.fun5()).toBe("object");
+    expect(functions.fun6()).toBe(undefined);
+});
+
+test ('testing section 3 stuff', () =>{
+    expect(functions.fun7(5)).toBe("odd");
+    expect(functions.fun7(4)).toBe("even");
+    expect(functions.fun8(4,4)).toBe(8);
+    expect(functions.fun8(6,4)).toBe(10);
+});
+
+test ('testing section 4 stuff', () =>{
+    expect(functions.fun9(0)).toBe(0);
+    expect(functions.fun9(1)).toBe(1);
+    expect(functions.fun10(1)).toBe(1);
+    expect(functions.fun10("geroge")).toBe("geroge");
+    expect(functions.fun11(1,1)).toBe(1);
+    expect(functions.fun11(2,3)).toBe(3); 
+});
+
+test ('testing section 5 stuff', () =>{
+    expect(functions.fun12(5)).toBe(15);
+    expect(functions.fun12(3)).toBe(6);
+    expect(functions.fun13(5)).toBe(5);
+    expect(functions.fun13(4)).toBe(4);
+    expect(functions.fun14(4)).toBe(10);
+    expect(functions.fun14(5)).toBe(15);
+    expect(functions.fun15(2)).toBe("12");
+    expect(functions.fun15(3)).toBe("123");
+    expect(functions.fun16(2)).toBe(2);
+    expect(functions.fun16(3)).toBe(3);
+});
+
+test ('testing section 2 stuff', () =>{
+    expect(functions.fun17()).toBe("jerry");
+    
+});
