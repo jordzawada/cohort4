46aeba373683648d60c10dd56eb224f59b68fbc0
J Zawada <jordzawada@gmail.com>
2020-03-04 13:44:00 -0700

    tried doing some DOM TDD

--- 02-dom/working with card/scripts/logic.js
+++ 02-dom/working with card/scripts/logic.js
@@ -3,14 +3,16 @@ const functions = {
     addCardBefore :(rightPanel) =>{
         let rightLength=document.getElementById(rightPanel).children.length;
         var node = document.createElement("div");
+
+
         var textnode = document.createTextNode("test");
         node.appendChild(textnode); 
-        let getEle = document.getElementById(x);
-        if (rightLength === 0) {
-                   
+
+    
+        let getEle = document.getElementById(rightPanel);
+        if (rightLength === 0) {        
             getEle.appendChild(node);
-        } else {
-                 
+        } else {     
             getEle.insertBefore(node,getEle.childNodes[0]);
             
         }
--- 02-dom/working with card/scripts/logic.test.js
+++ 02-dom/working with card/scripts/logic.test.js
@@ -1,9 +1,34 @@
 import functions from "./logic.js"
 
-test('Check add to right works', () => {
-    // expect(functions.addCardToRight("rightSide")).toBe(); 
-    
+test('Check that you can add a new div', () => {
+    document.body.innerHTML = 
+    `<div id="test">`+`</div>`;
 
+    function addDiv(node, text){
+        let div = document.createElement(node);
+        const textNode= document.createTextNode(text);
+        div.textnode;
+        return div;
+        }
 
+        // const div = document.createElement('div');
+        addDiv("div", 'one');
+        // console.log(document.getElementById("test"));  
+        expect().toBe(); 
+});
+
+test('Check that you can copy the contents of a specific ID`d div into', () => {
+    document.body.innerHTML = 
+    `<div id="test1">`
+    +`</div>`
+    + `<div id="test2">`
+    +`</div>`;
     
+    document.getElementById("test1").innerHTML= "test1";
+    
+    document.getElementById("test2").innerHTML= "test2";
+    // console.log(document.getElementById("test2").innerHTML); 
+
+    document.getElementById("test2").innerHTML =   document.getElementById("test1");
+    console.log(document.getElementById("test1"));  
 });
