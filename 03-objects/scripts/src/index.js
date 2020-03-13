d9a8cd9fc74eb16571b6dcefa684c5334871d50f
J Zawada <jordzawada@gmail.com>
2020-03-13 17:41:22 -0600

    can add a new account, asks to be named

--- 03-objects/scripts/src/index.js
+++ 03-objects/scripts/src/index.js
@@ -1,5 +1,27 @@
 import {Account,accountController} from './account.js';
 
+const usersAccounts = new accountController();
+
+const cloneAccountNode =()=>{
+    let toClone= document.getElementById("accountLower");
+    let newNode= toClone.cloneNode(true);
+    return newNode;
+}
+
+const addAnAccount = () => {
+   usersAccounts.addAccount("rename me");
+   let newAccountNode=cloneAccountNode();
+   let accountSection = document.getElementById("accountSections");
+   newAccountNode.getElementsByClassName("accountName")[0].textContent="New Account(Rename Me)";
+   newAccountNode.getElementsByClassName("balance")[0].textContent="Balance: "+0;
+
+   accountSection.appendChild(newAccountNode);
+
+
+}
+
+
+addAccounts.addEventListener('click',addAnAccount);
 
 
 
