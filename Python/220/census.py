091f09a63ab4b8ac42b248c65804b119836f615a
J Zawada <jordzawada@gmail.com>
2020-06-03 15:07:42 -0600

    I am a kind god

--- Python/220/census.py
+++ Python/220/census.py
@@ -3,4 +3,31 @@ census = open("Census2018.csv","r")
 
 census_f=csv.reader(census)
 
-print(next(census_f))
+totals = {
+    "total_Class":0,
+    "total_Sector":0,
+}
+
+def totalFunction():
+    Classes={}
+    Sectors ={}
+    for x in census_f:
+        # print( x[0])
+        if (x[0] == 'CLASS'):
+            # print(x[0])
+            continue
+        elif (x[0] not in Classes.keys() or x[4] not in Sectors.keys() ):
+            if (x[0] not in Classes.keys()):
+                Classes[x[0]] = int(x[9]);
+            if (x[4] not in Sectors.keys()  ):
+                Sectors[x[4]] = int(x[9]);
+        else:
+            Classes[x[0]] = Classes[x[0]] + int(x[9]);
+            Sectors[x[4]] = Sectors[x[4]] + int(x[9]);
+
+    totals["total_Class"] = Classes   
+    totals["total_Sector"] = Sectors
+
+print(totals["total_Class"])
+
+totalFunction()
